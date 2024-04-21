be2d1c90841831667792dfa39d13264b8af80315
yuluoyunyan <ww2048104136ww@163.com>
2024-04-22 01:12:23 +0800

    EFHttp-v0.0.8

--- EFHttp.js
+++ EFHttp.js
@@ -1,10 +1,32 @@
+/**
+ * 用于生成固定格式的响应对象
+ * @class Response
+ * @param {Object} options
+ * @param {Number} options.status 响应状态码
+ * @param {String} options.header 响应头
+ * @param {Object|String} options.data 响应数据
+ */
+class Response {
+  constructor(options) {
+    this.status = options.status;
+
+    let headerPairs = options.header.trim().split("\n");
+    let headerObj = {};
+    for (let i = 0; i < headerPairs.length; i++) {
+      let headerPair = headerPairs[i].replace(/\r/g, "").split(": ");
+      let headerName = headerPair[0];
+      let headerValue = headerPair[1];
+      headerObj[headerName] = headerValue;
+    }
+    this.header = headerObj;
+
+    this.data = JSON.parse(options.data);
+  }
+}
+
 export default class EFHttp {
   constructor() {
-    throw new Error(`
-        ESHttp不能实例化！
-        请使用get()、post()、put()、delete()静态方法发送请求；
-        以及使用ESHttp.defaultConfig属性修改默认配置。
-        `);
+    throw new Error(`ESHttp为静态类！`);
   }
   /**
    * 发送GET请求
@@ -74,32 +96,79 @@ export default class EFHttp {
    * @property {String} baseUrl 基础URL
    * @property {Object} header 请求头
    * @property {Boolean} isEncapsulationResponse 是否对响应数据进行封装
-   * @property {Object} interceptors 请求拦截器和响应拦截器
-   * @property {Array} interceptors.request 请求拦截器数组
-   * @property {Array} interceptors.response 响应拦截器数组
+   * @property {Array} requestInterceptors 请求拦截器数组
+   * @property {Array} responseInterceptors 响应拦截器数组
    */
-  static defaultConfig = {
+  static #defaultConf = {
     baseUrl: "",
     header: {},
     isEncapsulationResponse: true,
-    interceptors: {
-      request: [],
-      response: [],
-    },
+    requestInterceptors: [],
+    responseInterceptors: [],
   };
+  /**
+   * 默认配置代理
+   * @param {Object} config 配置对象
+   */
+  static defaultConfig = new Proxy(EFHttp.#defaultConf, {
+    // 拦截设置属性操作
+    set(target, prop, value) {
+      console.log("asdasd");
+      let defaultConfigTypeMap = {
+        baseUrl: "String",
+        header: "Object",
+        isEncapsulationResponse: "Boolean",
+        requestInterceptors: "Array",
+        responseInterceptors: "Array",
+      };
+      // 限制修改defaultConfig的子属性
+      if (prop in target) {
+        if (
+          Object.prototype.toString.call(value).slice(8, -1) !==
+          defaultConfigTypeMap[prop]
+        ) {
+          throw new Error(
+            `\n\n\t${prop}是${defaultConfigTypeMap[prop]}类型的哦~\n`
+          );
+        }
+        target[prop] = value;
+        return true;
+        // return Reflect.set(target, prop, value);
+      } else {
+        throw new Error(`\n\n您仅可对EFHttp.defaultConfig已有的子属性进行操作： \n\n${Object.keys(
+          EFHttp.#defaultConf
+        )
+          .map((key) => {
+            return "- " + key;
+          })
+          .join("\n")}
+        `);
+      }
+    },
+  });
+
+  /**
+   * 启动请求拦截器前，读取默认配置
+   * @param {Object} options 请求参数
+   */
   static #readDefaultConfig(options) {
-    if (EFHttp.defaultConfig.baseUrl.length !== 0) {
-      options.url = `${EFHttp.defaultConfig.baseUrl}${options.url}`;
+    if (EFHttp.#defaultConf.baseUrl.length !== 0) {
+      options.url = `${EFHttp.#defaultConf.baseUrl}${options.url}`;
     }
-    if (Object.keys(EFHttp.defaultConfig.header).length !== 0) {
+    if (Object.keys(EFHttp.#defaultConf.header).length !== 0) {
       options.header = {
-        ...EFHttp.defaultConfig.header,
+        ...EFHttp.#defaultConf.header,
         ...options.header,
       };
     }
   }
-  static #executeInterceptor(type, options) {
-    let InterceptorArr = EFHttp.defaultConfig.interceptors[type];
+  /**
+   * 执行请求/响应拦截器
+   * @param {String} interceptorsType 拦截器类型
+   * @param {Object} options 请求参数
+   */
+  static #executeInterceptors(interceptorsType, options) {
+    let InterceptorArr = EFHttp.#defaultConf[interceptorsType];
     if (InterceptorArr.length !== 0) {
       for (let key in InterceptorArr) {
         if (typeof InterceptorArr[key] === "function") {
@@ -108,10 +177,15 @@ export default class EFHttp {
       }
     }
   }
+  /**
+   * 发送HTTP请求
+   * @param {Object} options 请求参数
+   * @returns promise
+   */
   static #doHttp(options) {
     EFHttp.#readDefaultConfig(options);
 
-    EFHttp.#executeInterceptor("request", options);
+    EFHttp.#executeInterceptors("requestInterceptors", options);
 
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
@@ -130,7 +204,7 @@ export default class EFHttp {
         if (xhr.readyState === 4) {
           let response;
 
-          if(EFHttp.defaultConfig.isEncapsulationResponse){
+          if (EFHttp.#defaultConf.isEncapsulationResponse) {
             response = new Response({
               status: xhr.status,
               header: xhr.getAllResponseHeaders(),
@@ -138,7 +212,7 @@ export default class EFHttp {
             });
           }
 
-          EFHttp.#executeInterceptor("response", response);
+          EFHttp.#executeInterceptors("responseInterceptors", response);
 
           if (xhr.status >= 200 && xhr.status < 300) {
             resolve(response);
@@ -156,28 +230,8 @@ export default class EFHttp {
     });
   }
 }
-/**
- * 用于生成固定格式的响应对象
- * @class Response
- * @param {Object} options
- * @param {Number} options.status 响应状态码
- * @param {String} options.header 响应头
- * @param {Object|String} options.data 响应数据
- */
-class Response {
-  constructor(options) {
-    this.status = options.status;
 
-    let headerPairs = options.header.trim().split("\n");
-    let headerObj = {};
-    for (let i = 0; i < headerPairs.length; i++) {
-      let headerPair = headerPairs[i].replace(/\r/g, "").split(": ");
-      let headerName = headerPair[0];
-      let headerValue = headerPair[1];
-      headerObj[headerName] = headerValue;
-    }
-    this.header = headerObj;
-
-    this.data = JSON.parse(options.data);
-  }
-}
+// 冻结EFHttp静态类
+Object.freeze(EFHttp);
+// 密封EFHttp.defaultConfig属性
+Object.seal(EFHttp.defaultConfig);
--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ## EFHttp
 
-一个肥肠简单的 Http 请求库！（使用Axios不想看文档，就自己尝试写一个，哈哈哈）
+一个肥肠简单的 Http 请求库！（使用 Axios 不想看文档，就自己尝试写一个，哈哈哈）
 
 ## 声明
 
-- 仅支持 GET、POST、PUT、DELETE 请求，返回的是promise对象
-- GET请求的方法没有请求体哦！
+- 仅支持 GET、POST、PUT、DELETE 请求，返回的是 promise 对象
+- GET 请求的方法没有请求体哦！
 - 支持请求的默认配置
 - 支持请求&响应拦截
+- 暂未测试nodejs环境！
+- 本库由原生JS编写，不依赖任何第三方库
 
 ## 安装
 
@@ -18,7 +20,7 @@ npm i efhttp
 ## 使用
 
 ```
-// 仅url即可发送请求
+// 仅url亦可发送请求
 EFHttp.get({url:"https://www.baidu.com"})
 
 
@@ -37,21 +39,19 @@ EFHttp.post({
 
 // 默认配置属性
 EFHttp.defaultConfig：{
-    baseURL: "",        // 设置默认请求前缀
-    header: {},         // 设置默认请求头
-    isEncapsulationResponse: true,      // 是否使用默认配置封装响应信息（设置为false就得您老自己处理咯）
-    interceptors: {
-        request: [],    // 请求拦截器，这是数组嗷！执行顺序是数组头部到尾部的顺序
-        response: []    // 响应拦截器，同上
-    }
+    baseURL: "",                    // 设置默认请求前缀
+    header: {},                     // 设置默认请求头
+    isEncapsulationResponse: true,  // 是否使用默认配置封装响应信息（设置为false就得您老自己处理咯）
+    requestInterceptors: [],        // 请求拦截器，这是数组嗷！执行顺序是数组头部到尾部的顺序
+    responseInterceptors: []        // 响应拦截器，同上
 }
 
 
-// 请求拦截器，拦截器函数会接收请求对象，包括mehtod、url、header、body
-EFHttp.defaultConfig.interceptors.request.push(你的请求拦截器)
+// 请求拦截器，拦截器函数可接收请求对象作为参数，包括mehtod、url、header、body
+EFHttp.defaultConfig.requestInterceptors.push(你的请求拦截器)
 
-// 请求拦截器，拦截器函数会接收请求对象，包括mehtod、url、header、body
-EFHttp.defaultConfig.interceptors.response.push(你的响应拦截器)
+// 请求拦截器，拦截器函数可接收请求对象作为参数，包括mehtod、url、header、body
+EFHttp.defaultConfig.responseInterceptors.push(你的响应拦截器)
 ```
 
 ## 吐槽
